34f43d8f5fa51dcbf56a317e599f46cc3522d0a2
fujihyu <165942405+fujihyu@users.noreply.github.com>
2024-06-18 11:01:54 +0900

    Update start1.py

--- start1.py
+++ start1.py
@@ -1,4 +1,15 @@
 fuel = 10.0
 addition = 5.0
-total_fuel = fuel + addition
-total_fuel
+efficiency = 18.0
+distance = efficiency * total_fuel
+destination = 200
+def Fuel(fuel, addition):
+  fuel += addition
+  return fuel 
+def mobile(fuel, efficiency, distance):
+  
+def alert(destination, distance):
+  if destination > distance:
+    print("you need to add fuel!")
+  else:
+    print("you have enough fuel!")
